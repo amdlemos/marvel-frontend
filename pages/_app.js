f9a1fc31452d3a7c81870df131acba766d0226f7
alan <amdlemos@hotmail.com>
2021-12-19 20:45:18 -0300

    modified home page

--- pages/_app.js
+++ pages/_app.js
@@ -13,21 +13,7 @@ function MyApp({ Component, pageProps }) {
       <NavBar />
       <main className=''>
         <Component {...pageProps} />
-
-      </main>
-      {/* <footer className=''>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className=''>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
-
+      </main> 
     </div>
   )
 }
