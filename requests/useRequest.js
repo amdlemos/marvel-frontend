bdd6cfdeba34bea16f6b604896a6c7fc4671d808
alan <amdlemos@hotmail.com>
2021-12-19 20:46:54 -0300

    add request to characters

---
+++ requests/useRequest.js
@@ -0,0 +1,18 @@
+import useSWR from "swr"
+
+const fetcher = url => fetch(url).then(res => res.json())
+const baseUrl = "http://192.168.1.4:8080"
+
+export const useGetCharacters = path => {
+  if (!path) {
+    throw new Error("Path is required")
+  }
+
+  const url = baseUrl + path
+
+  const response = useSWR(url, fetcher)
+  
+  if(!response) console.log(response)
+
+  return response
+}
