530adcc93cb2f0d16c5cbac523567ace49006774
alan <amdlemos@hotmail.com>
2021-12-20 07:25:47 -0300

    remove tag a

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,7 @@ export default function Home() {
         </h1>     
       </main>
 
-      <footer className={styles.footer}>
-        <Link
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </Link>
+      <footer className={styles.footer}>    
       </footer>
     </div>
   )
